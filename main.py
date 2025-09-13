78d7112a5b03992c5f33b2d8cafd4aa60a30c7cf
Teemu Viikeri <tepavi@gmail.com>
2025-09-13 23:39:55 +0300

    Finish first version of Asteroids
    - Player can go forward, backward and rotate
    - Asteroids are being created on the screen
    - Collision detection for game over
    - Player can shoot
    - Collision detection for destroying asteroids with shots
    - Bigger asteroids splitting to two smaller asteroids

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+
+    initial_x = int(SCREEN_WIDTH / 2)
+    initial_y = int(SCREEN_HEIGHT / 2)
+    player = Player(initial_x, initial_y)
+
     running = True
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while running:
         for event in pygame.event.get():
@@ -15,7 +38,24 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+
+        updatable.update(dt)
+
+        for a in asteroids:
+            if a.collides_with(player):
+                print("Game over!")
+                import sys; sys.exit()
+
+            for s in shots:
+                if a.collides_with(s):
+                    a.split()
+                    s.kill()
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
